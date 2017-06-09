aacd65501016b10ae9dcb5c4821bd4d88c351af5
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-06-09 10:31:57 +0200

    Fix failed test

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -10,6 +10,10 @@ namespace Prime.Services
 		{
 			return false;
 		}
+		if (candidate == 2 )
+		{
+			return true;
+		}
 		throw new NotImplementedException("Please create a test first");
 	}
     }
